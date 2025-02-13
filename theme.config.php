52cdec58fd17895ddcaa2d28a500685ee031a8d5
Norbert Czirjak <norbert.czirjak@oeaw.ac.at>
2025-02-13 09:00:29 +0100

    resource view changes

--- theme.config.php
+++ theme.config.php
@@ -5,24 +5,11 @@ return [
     'helpers' => [
         'factories' => [
             'AkSearch\View\Helper\AkSearch\Warnings' => 'AkSearch\View\Helper\AkSearch\WarningsFactory',
-            'aksearchExt\View\Helper\Root\Record' => 'VuFind\View\Helper\Root\RecordFactory',
-            'aksearchExt\View\Helper\aksearchExt\Datepicker' => 'aksearchExt\View\Helper\aksearchExt\DatepickerFactory',
-            'aksearchExt\View\Helper\Root\AccountMenu' => 'AkSearch\View\Helper\Root\AccountMenuFactory',
-            'AkSearch\View\Helper\Root\Auth' => 'VuFind\View\Helper\Root\AuthFactory',
-            'AkSearch\View\Helper\Root\Citation' => 'VuFind\View\Helper\Root\CitationFactory',
-            'AkSearch\View\Helper\Root\SearchBox' => 'AkSearch\View\Helper\Root\SearchBoxFactory',
-            'VuFind\View\Helper\Root\RecordDataFormatter' => 'aksearchExt\View\Helper\Root\RecordDataFormatterFactory',
-            
+            #'AcdhchTheme\View\Helper\AcdhchTheme\Warnings' => 'AcdhchTheme\View\Helper\AcdhchTheme\WarningsFactory',
             ],
         'aliases' => [
             'warnings' => 'AkSearch\View\Helper\AkSearch\Warnings',
-            'record' => 'aksearchExt\View\Helper\Root\Record',
-            'datepicker' => 'aksearchExt\View\Helper\aksearchExt\Datepicker',
-            'accountMenu' => 'AkSearch\View\Helper\Root\AccountMenu',
-            'auth' => 'AkSearch\View\Helper\Root\Auth',
-            'citation' => 'AkSearch\View\Helper\Root\Citation',
-            'searchbox' => 'AkSearch\View\Helper\Root\SearchBox',
-            
+            #'warnings' => 'AcdhchTheme\View\Helper\AcdhchTheme\Warnings',
         ],
     ],
     /**
